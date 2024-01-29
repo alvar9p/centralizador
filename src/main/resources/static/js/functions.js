8896d760f2ea7bcc5a1e9c0a00039e4d99f86920
Alvaro Poblete <y5u@economical.com>
2024-01-29 16:25:30 -0300

    Code refactor - Disease update

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,84 +1,87 @@
-// JQuery para modifyDisease
-$(document).ready(function () {
-  $("#symptomName").change(function () {
-    var selectedSymptomId = $(this).val();
+function updateCaresAndConsiderations(index) {
+  var selectedSymptomId = $("#symptomSelect" + index).val();
 
-    // Realizar solicitud AJAX al servidor
-    $.ajax({
-      type: "GET",
-      url: "/create/getCaresAndConsiderations/" + selectedSymptomId, // Ajusta la URL según tu configuración
-      success: function (response) {
-        // Actualizar la interfaz con los cuidados y consideraciones
-        $("#cares").text(response.cares);
-        $("#caresInput").val(response.cares);
+  // Obtener los valores de cares y considerations del elemento seleccionado
+  var selectedOption = $("#symptomSelect" + index + " option:selected");
+  var cares = selectedOption.data('cares');
+  var considerations = selectedOption.data('considerations');
 
-        $("#considerations").text(response.considerations);
-        $("#considerationsInput").val(response.considerations);
-      },
-      error: function () {
-        console.log("Error al obtener cuidados y consideraciones.");
-      },
-    });
-  });
-});
-
+  // Actualizar los campos cares y considerations
+  $("#caresDisplay" + index).val(cares);
+  $("#considerationsDisplay" + index).val(considerations);
+}
 
 function generarFormulario() {
-    var cantidadDeEtapas = document.getElementById("numberOfStages").value;
-    var formularioEtapas = document.getElementById("stagesForm");
+  var cantidadDeEtapas = document.getElementById("numberOfStages").value;
+  var formularioEtapas = document.getElementById("stagesForm");
 
-    formularioEtapas.innerHTML = ""; // Limpiar el formulario existente
+  formularioEtapas.innerHTML = ""; // Limpiar el formulario existente
 
     if (cantidadDeEtapas > 0) {
-        for (var i = 1; i <= cantidadDeEtapas; i++) {
-            var divEtapa = document.createElement("div");
-            var nombreEtapa = "ETAPA_0" + i;
-            divEtapa.innerHTML = `
-              <hr>
-              <h3>Trabajando en Etapa ${i}</h3>
+      for (var i = 1; i <= cantidadDeEtapas; i++) {
+        var divEtapa = document.createElement("div");
+        var nombreEtapa = "ETAPA_0" + i;
 
-              <div class="col-6 mt-3">
+        divEtapa.innerHTML = `
+            <hr>
 
-                  <div class="form-floating mb-3">
-                      <input class="form-control" type="hidden" name="diseaseStage[${i - 1}].StageID" value="${nombreEtapa}" readonly />
-                      <label for="diseaseStage[${i - 1}].StageID" style="visibility: hidden;">Nombre Etapa ${i}:</label>
-                  </div>
+            <div class="row">
 
-                  <div class="form-floating mb-3">
-                        <input class="form-control" type="text" name="diseaseStage[${i - 1}].stageIdentifier" />
-                        <label for="diseaseStage[${i - 1}].stageIdentifier">Nombre</label>
-                  </div>
+                <h3>Trabajando en Etapa ${i}</h3>
 
-                  <div class="form-floating mb-3">
-                        <input class="form-control" type="text" name="diseaseStage[${i - 1}].stageDesc" />
-                        <label for="diseaseStage[${i - 1}].stageDesc">Descripcion</label>
-                  </div>
+                <div class="col-6 mt-3">
 
-                  <div class="form-floating mb-3">
-                        <input class="form-control" type="number" name="diseaseStage[${i - 1}].duration" />
-                        <label for="diseaseStage[${i - 1}].duration">Duracion</label>
-                  </div>
+                      <div class="form-floating mb-3">
+                          <input class="form-control" type="hidden" name="diseaseStage[${i - 1}].StageID" value="${nombreEtapa}" readonly />
+                          <label for="diseaseStage[${i - 1}].StageID" style="visibility: hidden;">Nombre Etapa ${i}:</label>
+                      </div>
+                      <div class="form-floating mb-3">
+                            <input class="form-control" type="text" name="diseaseStage[${i - 1}].stageIdentifier" />
+                            <label for="diseaseStage[${i - 1}].stageIdentifier">Nombre</label>
+                      </div>
+                      <div class="form-floating mb-3">
+                            <input class="form-control" type="text" name="diseaseStage[${i - 1}].stageDesc" />
+                            <label for="diseaseStage[${i - 1}].stageDesc">Descripcion</label>
+                      </div>
+                      <div class="form-floating mb-3">
+                            <input class="form-control" type="number" name="diseaseStage[${i - 1}].duration" />
+                            <label for="diseaseStage[${i - 1}].duration">Duracion</label>
+                      </div>
 
-              </div>
+                </div>
 
-              <div class="col-6 mt-3">
+                <div class="col-6 mt-3">
 
-                   <div class="form-floating mb-3">
-                        <p>Texto </p>
-                   </div>
+                    <div class="form-floating mb-3">
+                        <select class="form-control" type="text" name="diseaseStage[${i - 1}].symptomName" id="symptomSelect${i}" onchange="updateCaresAndConsiderations(${i})">
+                        <option value="">Selecciona un síntoma...</option>
+                            ${symptoms.map((symptom) => `<option value="${symptom.idSymptom}"
+                            data-cares="${symptom.cares}"
+                            data-considerations="${symptom.considerations}">${symptom.symptomName}</option>`).join("")}
+                         </select>
+                         <label for="diseaseStage[${i - 1}].symptomName" class="form-label">Nombre del síntoma</label>
+                    </div>
 
 
+                    <div class="form-floating mb-3">
+                        <input class="form-control" type="text" id="caresDisplay${i}" name="diseaseStage[${i - 1}].symptomCare" >
+                        <label for="diseaseStage[${i - 1}].symptomCare" class="form-label">Cares:</label>
+                    </div>
 
-              </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" type="text" id="considerationsDisplay${i}" name="diseaseStage[${i - 1}].symptomConsideration" >
+                        <label for="diseaseStage[${i - 1}].symptomConsideration" class="form-label">Considerations:</label>
+                    </div>
 
+                </div>
 
-              <br>
-              <hr>
+                <br>
+            </div>
 
-      `;
-      formularioEtapas.appendChild(divEtapa);
-    }
-  }
-
-  console.log("Cantidad de etapas selecionada: " + cantidadDeEtapas);
+            <hr>
+            `;
+        formularioEtapas.appendChild(divEtapa);
+        }
+      }
+    console.log("Cantidad de etapas selecionada: " + cantidadDeEtapas);
 }
